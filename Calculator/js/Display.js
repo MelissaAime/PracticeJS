af48a15cfc7f352ea655eea7f21bcf190517c053
Melissa Aime <aimemelissa1@gmail.com>
2023-01-19 11:58:24 -0300

    calculator - delete button

--- Calculator/js/Display.js
+++ Calculator/js/Display.js
@@ -3,8 +3,21 @@ class Display {
         this.displayValueActual = displayValueActual;
         this.displayValuePrev = displayValuePrev;
         this.calculator = new Calculator();
+        this.typeOperator = undefined;
         this.valueActual = '';
         this.valuePrev = '';
+    }
+
+    delete(){
+        this.valueActual = this.valueActual.toString().slice(0,-1);
+        this.printValue();
+    }
+
+    deleteAll(){
+        this.valueActual = '';
+        this.valuePrev = '';
+        this.typeOperator = undefined;
+        this.printValue();
     }
 
     addNumber(number) {
