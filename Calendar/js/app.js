1739b8bcbafa4107c08643d51cf0d6c673eb2761
Melissa Aime <aimemelissa1@gmail.com>
2023-01-31 10:56:08 -0300

    calendar - select actual day

--- Calendar/js/app.js
+++ Calendar/js/app.js
@@ -1,5 +1,18 @@
 const date = new Date();
+
+date.setDate(1);
+
 const monthDays = document.querySelector('.days');
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const firstDayIndex = date.getDay();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+
+const nextDays = 7 - lastDayIndex - 1;
 
 const months = [
     "January",
@@ -22,7 +35,19 @@ document.querySelector('.date p').innerHTML = date.toDateString();
 
 let days = "";
 
-for(let i = 1; i <= 31; i++){
-    days += `<div>${i}</div>`;
+for(let x = firstDayIndex; x > 0; x--){
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+};
+
+for(let i = 1; i <= lastDay; i++){
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+        days += `<div class="today">${i}</div>`;
+    } else {
+        days += `<div>${i}</div>`;
+    }
+};
+
+for(let j = 1; j <= nextDays; j++){
+    days += `<div class="next-date">${j}</div>`;
     monthDays.innerHTML = days;
 };
