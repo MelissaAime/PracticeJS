102abf4878668a8dc398ac50d63f127f48eeb6f2
Melissa Aime <aimemelissa1@gmail.com>
2023-01-31 13:48:10 -0300

    change color

--- LoginForm/js/app.js
+++ LoginForm/js/app.js
@@ -9,13 +9,13 @@ password.addEventListener('textInput', pass_Verify);
 
 function validated(){
 	if (email.value.length < 9) {
-		email.style.border = "1px solid red";
+		email.style.border = "1px solid";
 		email_error.style.display = "block";
 		email.focus();
 		return false;
 	}
 	if (password.value.length < 6) {
-		password.style.border = "1px solid red";
+		password.style.border = "1px solid";
 		pass_error.style.display = "block";
 		password.focus();
 		return false;
