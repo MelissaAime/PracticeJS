42d803170e4088795d5c5b9077409cdabd3b5614
Melissa Aime <aimemelissa1@gmail.com>
2023-02-01 12:07:10 -0300

    add images

--- app.js
+++ app.js
@@ -3,91 +3,91 @@ const projects = [
         "name": "Gallery",
         "id": 1,
         "src": "./Gallery/index.html",
-        "img": "",
+        "img": "./assets/images/01.png",
         "content": ""
     },
     {
         "name": "Weather",
         "id": 2,
         "src": "./Weather/index.html",
-        "img": "",
+        "img": "./assets/images/02.png",
         "content": ""
     },
     {
         "name": "AnalogClock",
         "id": 3,
         "src": "./AnalogClock/index.html",
-        "img": "",
+        "img": "./assets/images/03.png",
         "content": ""
     },
     {
         "name": "Calculator",
         "id": 4,
         "src": "./Calculator/index.html",
-        "img": "",
+        "img": "./assets/images/04.png",
         "content": ""
     },
     {
         "name": "DarkMode",
         "id": 5,
         "src": "./DarkMode/",
-        "img": "",
+        "img": "./assets/images/05.png",
         "content": ""
     },
     {
         "name": "TaskList",
         "id": 6,
         "src": "./Task/index.html",
-        "img": "",
+        "img": "./assets/images/06.png",
         "content": ""
     },
     {
         "name": "LoginForm",
         "id": 7,
         "src": "./LoginForm/index.html",
-        "img": "",
+        "img": "./assets/images/07.png",
         "content": ""
     },
     {
-        "name": "Time",
+        "name": "Timer",
         "id": 8,
-        "src": "./Time/index.html",
-        "img": "",
+        "src": "./Timer/index.html",
+        "img": "./assets/images/08.png",
         "content": ""
     },
     {
         "name": "CountDown",
         "id": 9,
         "src": "./CountDown/index.html",
-        "img": "",
+        "img": "./assets/images/09.png",
         "content": ""
     },
     {
         "name": "Calendar",
         "id": 10,
         "src": "./Calendar/index.html",
-        "img": "",
+        "img": "./assets/images/10.png",
         "content": ""
     },
     {
         "name": "ImageHover",
         "id": 11,
         "src": "./ImageHover/index.html",
-        "img": "",
+        "img": "./assets/images/11.png",
         "content": ""
     },
     {
         "name": "MenuDesign",
         "id": 12,
         "src": "./MenuDesign/index.html",
-        "img": "",
+        "img": "./assets/images/12.png",
         "content": ""
     },
     {
         "name": "GoogleMapClone",
         "id": 13,
         "src": "./GoogleMapClone/index.html",
-        "img": "",
+        "img": "./assets/images/13.png",
         "content": ""
     }
 ]
@@ -96,9 +96,9 @@ let card = document.getElementById('card-template');
 
 projects.map((x)=>{
     card.innerHTML += `
-    <div class="card"
+    <div class="card py-1"
         <div>
-            <img src="" class="card-img-top" alt="${x.name}">
+            <img src="${x.img}" class="card-img-top" alt="${x.name}">
             <div class="card-body">
             <h5 class="card-title">${x.id}. ${x.name}</h5>
             <p class="card-text py-0">${x.content}</p>
