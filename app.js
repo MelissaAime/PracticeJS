5c67f7602aa74edd139cb20c7f84d726e9a6840c
Melissa Aime <aimemelissa1@gmail.com>
2023-02-02 12:19:03 -0300

    add button home

--- app.js
+++ app.js
@@ -2,91 +2,91 @@ const projects = [
     {
         "name": "Gallery",
         "id": 1,
-        "src": "./Gallery/index.html",
+        "src": "./01. Gallery/index.html",
         "img": "./assets/images/01.png",
         "content": ""
     },
     {
         "name": "Weather",
         "id": 2,
-        "src": "./Weather/index.html",
+        "src": "./02. Weather/index.html",
         "img": "./assets/images/02.png",
         "content": ""
     },
     {
         "name": "AnalogClock",
         "id": 3,
-        "src": "./AnalogClock/index.html",
+        "src": "./03. AnalogClock/index.html",
         "img": "./assets/images/03.png",
         "content": ""
     },
     {
         "name": "Calculator",
         "id": 4,
-        "src": "./Calculator/index.html",
+        "src": "./04. Calculator/index.html",
         "img": "./assets/images/04.png",
         "content": ""
     },
     {
         "name": "DarkMode",
         "id": 5,
-        "src": "./DarkMode/",
+        "src": "./05. DarkMode/",
         "img": "./assets/images/05.png",
         "content": ""
     },
     {
         "name": "TaskList",
         "id": 6,
-        "src": "./Task/index.html",
+        "src": "./06. Task/index.html",
         "img": "./assets/images/06.png",
         "content": ""
     },
     {
         "name": "LoginForm",
         "id": 7,
-        "src": "./LoginForm/index.html",
+        "src": "./07. LoginForm/index.html",
         "img": "./assets/images/07.png",
         "content": ""
     },
     {
         "name": "Timer",
         "id": 8,
-        "src": "./Timer/index.html",
+        "src": "./08. Timer/index.html",
         "img": "./assets/images/08.png",
         "content": ""
     },
     {
         "name": "CountDown",
         "id": 9,
-        "src": "./CountDown/index.html",
+        "src": "./09. CountDown/index.html",
         "img": "./assets/images/09.png",
         "content": ""
     },
     {
         "name": "Calendar",
         "id": 10,
-        "src": "./Calendar/index.html",
+        "src": "./10. Calendar/index.html",
         "img": "./assets/images/10.png",
         "content": ""
     },
     {
         "name": "ImageHover",
         "id": 11,
-        "src": "./ImageHover/index.html",
+        "src": "./11. ImageHover/index.html",
         "img": "./assets/images/11.png",
         "content": ""
     },
     {
         "name": "MenuDesign",
         "id": 12,
-        "src": "./MenuDesign/index.html",
+        "src": "./12. MenuDesign/index.html",
         "img": "./assets/images/12.png",
         "content": ""
     },
     {
         "name": "GoogleMapClone",
         "id": 13,
-        "src": "./GoogleMapClone/index.html",
+        "src": "./13. GoogleMapClone/index.html",
         "img": "./assets/images/13.png",
         "content": ""
     }
